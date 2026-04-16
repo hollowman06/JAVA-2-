963ea9a7eb92166c6d349ec2f0965de430198016
dongg <donggeun06@gmail.com>
2026-04-16 23:08:22 +0900

    260408

--- start.java
+++ start.java
@@ -1,8 +1,22 @@
-public class start{
-    public static void main(string[]arg){
-        out.println("hello world");
+class Circle{
+    int radius;
 
-        int i = 0;
+    public Circle(int radius){
+        this.radius = radius;
+    }
+    public double getArea(){
+        return 3.14 = radius*radius;
+    }
+}
+public class start {
+    public static void main(String[] args){
+        Circle[] c;
+        c = new Circle[5];
 
+        for (int i=0;i<c.length; i++)
+            c[i] = new Circle(i);
+
+        for (int i = 0; i<c.length; i++)
+            System.out.print((int) (c[i] getArea()) + " ");
     }
 }
